f85ca7933f493a188e8b1b0eb7472222d12750d9
Taylor McKinnon <mail@tmacs.space>
2020-03-05 14:41:34 -0800

    fix s3c version conversion

--- notomaton/util/ticket.py
+++ notomaton/util/ticket.py
@@ -9,4 +9,4 @@ def parse_version(ver):
 
 def ring_to_s3c_version(ver):
     ring_ver = parse_version(ver)
-    return '.'.join(map(str, [7, *ring_ver[1:]]))
+    return '.'.join(map(str, [7, ring_ver[1] + 5, *ring_ver[2:]]))
